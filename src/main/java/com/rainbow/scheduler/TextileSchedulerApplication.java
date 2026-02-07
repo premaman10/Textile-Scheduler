1eec5d6aa23ee7d3cebb56778e2542bca7a1ff59
premaman <119154881+premaman10@users.noreply.github.com>
2026-02-07 10:11:09 +0530

    initialize spring boot project with dependencies and configuration

---
+++ src/main/java/com/rainbow/scheduler/TextileSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.rainbow.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextileSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TextileSchedulerApplication.class, args);
+    }
+
+}
